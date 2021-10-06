6457ec531ed99a374d2678d0ecebf70604a7b27c
Thanh Ngoc <ngochc1@gmail.com>
2021-10-06 22:44:27 +0700

    fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -32,7 +32,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = catchAsync(async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 });
 exports.webhookCheckout = catchAsync(async (req, res, next) => {
