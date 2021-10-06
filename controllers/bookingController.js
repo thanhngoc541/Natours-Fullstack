4b5b8eae8baf5bca6064af9cf2ed0c01ce95d306
Thanh Ngoc <ngochc1@gmail.com>
2021-10-06 22:35:06 +0700

    fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -43,7 +43,7 @@ exports.webhookCheckout = catchAsync(async (req, res, next) => {
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
-  if (event.type === 'checkout.session.complete') createBookingCheckout(event.data.object);
+  if (event.type === 'checkout.session.completed') createBookingCheckout(event.data.object);
   res.status(200).json({ received: true });
 });
 exports.getAllBookings = factory.getAll(Booking);
