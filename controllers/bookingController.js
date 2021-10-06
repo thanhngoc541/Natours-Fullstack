ab0a404337d5b66e60cd76a7ac118b6cd18f68aa
Thanh Ngoc <ngochc1@gmail.com>
2021-10-06 22:36:18 +0700

    fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -17,7 +17,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: ['https://www.natours.dev/img/tours/tour-1-cover.jpg'],
+        images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1,
